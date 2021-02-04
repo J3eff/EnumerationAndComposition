28852130f82aed116a86cd3598dcfca3e27b87ad
Jefferson Brandão <jeffersonvideo125@gmail.com>
2021-02-04 00:36:15 -0300

    Exercicio de Enumerações e Composição, aplicando tudo que foi ensinado no capitulo

---
+++ EnumerationAndComposition/Entities/Order.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Globalization;
+using EnumerationAndComposition.Entities.Enums;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnumerationAndComposition.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+        }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public double Total()
+        {
+            double sum = 0.0;
+            foreach(OrderItem items in Items)
+            {
+                sum += items.SubTotal();
+            }
+
+            return sum;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Order moment: " + Moment.ToString("dd/MM/yyyy HH:mm:ss"));            
+            sb.Append("Order status: " + Status);
+            sb.AppendLine("Client: " + Client);            
+            sb.AppendLine("Order items: ");
+            foreach(OrderItem items in Items)
+            {
+                sb.AppendLine(items.ToString());
+            }
+            sb.AppendLine("Total price: $" + Total().ToString("F2", CultureInfo.InvariantCulture));
+            return sb.ToString();
+        }
+    }
+}
